34681bdccf33864c9ff091220572b385d3a5186d
LUDcifer <desmonmiles8996@gmail.com>
2018-03-30 16:37:52 +0700

    update Lab 3- Guest it game>>completed|| Calc>>Not complete

--- Lab3/Lab3.js
+++ Lab3/Lab3.js
@@ -59,8 +59,7 @@ function xuli(e) {
     else {
 
         pB = result;
-        if (pB >= 10) 
-        {   //Prob comeup when number over 3 digit
+        if (pB >= 10) {   //Prob comeup when number over 3 digit
             pA = parseInt(pB) + tempCacl;
             console.log("pB:  " + pB);
             console.log("pA ritenao:  " + pA);
@@ -115,7 +114,7 @@ function xuliop(e) {
                 document.getElementById("show_result").innerHTML = tempCacl;
                 break;
             case "/":
-                tempCacl = parseInt(pA)/parseInt(pB);
+                tempCacl = parseInt(pA) / parseInt(pB);
                 document.getElementById("show_result").innerHTML = tempCacl;
                 break;
             default: break;
@@ -127,3 +126,117 @@ function xuliop(e) {
 
     }
 }
+
+// Guest It Game
+var b2A, b2B, XXX;
+var count = 0;
+var result = " ";
+
+//func randoom
+function RandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+//
+//
+//
+var randN, cond = "new";
+function game() {
+
+    //      
+    //
+    //
+    //Kiem tra xem la lan New Game hay la Restart
+
+    if (cond == "new") {
+        b2A = prompt("Nhap A: ");
+        b2B = prompt("Nhap B: ");
+        check();
+        randN = RandomInt(b2A, b2B);
+        console.log("rand" + randN);
+        cond = "process";
+    } else {
+        cond = "process";
+        console.log("process cond check first: " + cond);
+    }
+
+    //Get user input and process
+    //
+    //
+
+    XXX = prompt("Ban doan so may: ");
+    document.getElementById("usrinp").innerHTML = ("Random from " + "<b>" + b2A + "</b>" + " to " + "<b>" + b2B + "</b>" + "<br>" + " User Selection is: " + "<b>" + XXX + "</b>");
+
+    //Process
+    //
+    //
+    if (XXX == randN) {
+        result = (" Congraz, You win !, Randnumb is: " + randN);
+        document.getElementById("rslt").innerHTML = result;
+        cond = "win";
+    }
+    else {
+
+
+        if (XXX > randN) {
+            result = "RandNumb is Smaller, play again?" + "<br><br>" + "<b>" + (4 - count) + " Attempt left";
+            document.getElementById("rslt").innerHTML = result;
+            console.log("process cond check 2nd: " + cond);
+        }
+        else {
+            result = "RandNumb is Bigger, play again? " + "<br><br>" + "<b>" + (4 - count) + " Attempt left";
+            document.getElementById("rslt").innerHTML = result;
+            console.log("process cond check 2nd: " + cond);
+        }
+
+    }
+}
+
+// Playagain or Start New ( On first Click)
+
+function repeat() {
+    if (cond == "win" || cond == "new") {
+        reset();
+        game();
+
+    }
+    else {
+        if (count <= 3) {
+            count += 1;
+            game();
+        }
+        else {
+            result = ("LOSE, dude !, randNumb is:" + "<b>" + randN + "</b>");
+            document.getElementById("rslt").innerHTML = result;
+            console.log(count);
+            reset();
+        }
+    }
+}
+
+//Reset state
+//
+//
+function reset() {
+    cond = "new";
+    count = 1;
+    randN = 0;
+    b2A = b2B = 0;
+    result = "";
+}
+
+
+//check if input is not a Number 
+//
+//
+//
+function check() {
+    if (isNaN(b2A) || isNaN(b2B)) {
+        alert("Number Only Dude !!!!!!!!!!!!!!!");
+        throw new Error("Input Number Only pleaz !");
+    }
+    else {
+        b2A = Math.abs(b2A);
+        b2B = Math.abs(b2B);
+    }
+}
+
